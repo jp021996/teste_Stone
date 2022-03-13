ae7a7e91e268c0d54906ae6ccdece9517fc13022
unknown <jp021996@gmail.com>
2022-03-13 19:27:23 -0300

    Adding data to postgresql

--- dags/migracao.py
+++ dags/migracao.py
@@ -12,8 +12,7 @@ import datetime
 BIGQUERYCONN = 'Big_Query_Conn'
 POSTGRESQLCONN = 'PostgreSQL'
 
-def migrate_data_function(date):
-    pg_hook = PostgresHook(POSTGRESQLCONN)
+def create_table(pg_hook):
     with pg_hook.get_conn() as conn:
         with conn.cursor() as curs: 
             query = """
@@ -29,25 +28,53 @@ def migrate_data_function(date):
                     block_hash VARCHAR (255) NOT NULL
                 )
             """
+            logging.info('Creating table if it doesnt exist')
             curs.execute(query)
-            query = """
-                    SELECT *
-                FROM pg_catalog.pg_tables
-                WHERE schemaname != 'pg_catalog' AND 
-                    schemaname != 'information_schema';
-            """
-            curs.execute(query)
+            # query = """
+            #         SELECT *
+            #     FROM pg_catalog.pg_tables
+            #     WHERE schemaname != 'pg_catalog' AND 
+            #         schemaname != 'information_schema';
+            # """
+            # curs.execute(query)
             source = curs.fetchall()
-            logging.info(source)
-    # bq_hook = BigQueryHook(gcp_conn_id=BIGQUERYCONN)
-    # client = bq_hook.get_client()
-    # logging.info(f'Data: {date}')
-    # QUERY = f"""
-    #     SELECT * FROM `bigquery-public-data.crypto_ethereum.tokens` WHERE CAST( block_timestamp as DATE) = '{date}'
-    # """
-    # data = client.query(QUERY)
-    # for row in data:
-    #     logging.info(row)   
+            # logging.info(source)
+
+def insert_data(pg_hook, row):
+    with pg_hook.get_conn() as conn:
+        with conn.cursor() as curs:
+            logging.info('Inserting row of data in postgresql')
+            query = f"""
+                INSERT INTO postgres.public.tokens 
+                (address, symbol, name, decimals, total_supply, block_timestamp, block_number, block_hash)
+                VALUES ({row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]})
+            """
+            logging.info(query)
+            # curs.execute(query)
+            # query = """
+            #         SELECT *
+            #     FROM pg_catalog.pg_tables
+            #     WHERE schemaname != 'pg_catalog' AND 
+            #         schemaname != 'information_schema';
+            # """
+            # curs.execute(query)
+            # source = curs.fetchall()
+            # logging.info(source)
+
+def migrate_data_function(date):
+    pg_hook = PostgresHook(POSTGRESQLCONN)
+    
+    create_table(pg_hook)
+    bq_hook = BigQueryHook(gcp_conn_id=BIGQUERYCONN)
+    client = bq_hook.get_client()
+    logging.info(f'Data: {date}')
+    QUERY = f"""
+        SELECT * FROM `bigquery-public-data.crypto_ethereum.tokens` WHERE CAST( block_timestamp as DATE) = '{date}'
+    """
+    logging.info(f'Getting data from BigQuery')
+    data = client.query(QUERY)
+    for row in data:
+        logging.info(row)   
     
 
 
